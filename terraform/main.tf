4401d2db96375adc5f61b8b7dde44a159bb1482f
mulykoV <144117863+mulykoV@users.noreply.github.com>
2024-11-24 19:50:06 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,11 @@
-terraform {
-  required_version = ">=0.13.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS provider
-provider "aws" {
-  region     = "eu-central-1"
-}
-
 resource "aws_security_group" "web_app" {
   name        = "web_app"
   description = "security group"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
   ingress {
     from_port   = 80
     to_port     = 80
@@ -23,12 +13,13 @@ resource "aws_security_group" "web_app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 65535
@@ -36,21 +27,7 @@ resource "aws_security_group" "web_app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags= {
+  tags = {
     Name = "web_app"
   }
-}
-
-resource "aws_instance" "webapp_instance" {
-  ami           = "ami-0669b163befffbdfc"
-  instance_type = "t2.micro"
-  security_groups= ["web_app"]
-  tags = {
-    Name = "webapp_instance"
-  }
-}
-
-output "instance_public_ip" {
-  value     = aws_instance.webapp_instance.public_ip
-  sensitive = true
 }
