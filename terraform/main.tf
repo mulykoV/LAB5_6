eb874d0ecbbbce35213e212d6385b3814ca1a0f3
mulykoV <144117863+mulykoV@users.noreply.github.com>
2024-11-27 19:26:46 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-# Terraform конфігурація
 terraform {
   required_version = ">=0.13.0"
 
@@ -9,7 +8,7 @@ terraform {
     }
   }
 
-  # Backend конфігурація для S3 та DynamoDB
+  # Backend configuration для S3 та DynamoDB
   backend "s3" {
     bucket         = "lab6-7ter-form"
     key            = "terraform.tfstate"
@@ -22,51 +21,6 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-# Створення ресурсу Lightsail Container Service
-resource "aws_lightsail_container_service" "flask_application" {
-  name                   = "flask-app"
-  power                  = "nano"
-  scale                  = 1
-  private_registry_access {
-    ecr_image_puller_role {
-      is_active = true
-    }
-  }
-
-  tags = {
-    version = "1.0.0"
-  }
-}
-
-# Версія розгортання
-resource "aws_lightsail_container_service_deployment_version" "flask_app_deployment" {
-  container {
-    container_name = "flask-application"
-    image          = "${var.REPOSITORY_URI}:latest"
-
-    ports {
-      container_port = 8080
-      protocol       = "HTTP"
-    }
-
-    public_endpoint {
-      container_name = "flask-application"
-      container_port = 8080
-      health_check {
-        healthy_threshold   = 2
-        unhealthy_threshold = 2
-        timeout_seconds     = 5
-        interval_seconds    = 5
-        path                = "/"
-        success_codes       = "200-499"
-      }
-    }
-  }
-}
-
-service_name = aws_lightsail_container_service.flask_application.name
-
-# Security Group для Web App
 resource "aws_security_group" "web_app" {
   name        = "web_app"
   description = "Security group for web app"
@@ -101,10 +55,4 @@ resource "aws_security_group" "web_app" {
   tags = {
     Name = "web_app"
   }
-}
-
-# Змінна для репозиторію
-variable "REPOSITORY_URI" {
-  type = string
-  default = "python_app_repository"
 }
