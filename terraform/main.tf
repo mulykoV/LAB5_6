ddca414158b73247ec116f25e443a423e0375beb
mulykoV <144117863+mulykoV@users.noreply.github.com>
2024-11-28 02:04:01 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ terraform {
     bucket         = "lab6-7ter-form"
     key            = "terraform.tfstate"
     region         = "ap-northeast-1"
-    dynamodb_table = "lab-my-tf-lockid" # Назва DynamoDB таблиці для lock-файлів
+    dynamodb_table = "lab-my-tf-lockid"
   }
 }
 
@@ -27,39 +27,49 @@ variable "REPOSITORY_URI" {
   type        = string
 }
 
-# Ресурс для безпеки: створення Security Group
-resource "aws_security_group" "web_app" {
-  name        = "web_app"
-  description = "Security group for web app"
+# Ресурс для Lightsail контейнерного сервісу
+resource "aws_lightsail_container_service" "flask_app" {
+  name        = "flask-app"
+  power       = "nano"   # Тип сервісу (nano, micro, small і т.д.)
+  scale       = 1        # Кількість вузлів
+  is_active   = true
 
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  # Правила вхідного трафіку (ingress)
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Правила вихідного трафіку (egress)
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  private_registry_access {
+    ecr_image_puller_role = true
   }
 
   tags = {
-    Name = "web_app"
+    Environment = "production"
+    Project     = "Lab6"
+  }
+}
+
+# Ресурс для розгортання додатку
+resource "aws_lightsail_container_service_deployment_version" "flask_app_deployment" {
+  container_service_name = aws_lightsail_container_service.flask_app.name
+
+  container {
+    image = "${var.REPOSITORY_URI}:latest" # Використовує останню версію імеджа
+    command = []  # Можна додати команди для запуску контейнера
+    environment = {
+      APP_ENV = "production"
+    }
+    ports = {
+      "80" = "HTTP"
+    }
+  }
+
+  public_endpoint {
+    container_name = "flask_app"
+    container_port = 80
+
+    health_check {
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      timeout_seconds     = 2
+      interval_seconds    = 5
+      path                = "/"
+      success_codes       = "200-499"
+    }
   }
 }
