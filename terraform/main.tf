719adda9700a331d07b8c48752d902dba9562eac
mulykoV <144117863+mulykoV@users.noreply.github.com>
2024-11-28 02:08:09 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,55 +21,49 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-# Оголошення змінної для REPOSITORY_URI
 variable "REPOSITORY_URI" {
-  description = "The URI of the Docker repository"
-  type        = string
+  type = string
+  description = "Опис змінної з назвою реєстру образів"  # Red label
 }
 
-# Ресурс для Lightsail контейнерного сервісу
-resource "aws_lightsail_container_service" "flask_app" {
-  name        = "flask-app"
-  power       = "nano"   # Тип сервісу (nano, micro, small і т.д.)
-  scale       = 1        # Кількість вузлів
-  is_active   = true
-
+resource "aws_lightsail_container_service" "flask_application" {
+  name = "flask-application"
+  power = "nano"  # Тип виводу
+  scale = 1
   private_registry_access {
-    ecr_image_puller_role = true
+    ecr_image_puller_role = true  # Опис прав доступу до приватного репозиторію у вигляді ролі ecr_image_puller_role
+    is_active = true
   }
-
   tags = {
-    Environment = "production"
-    Project     = "Lab6"
+    version = "1.0.0"
   }
 }
 
-# Ресурс для розгортання додатку
 resource "aws_lightsail_container_service_deployment_version" "flask_app_deployment" {
-  container_service_name = aws_lightsail_container_service.flask_app.name
+  container_name = "flask-application"
+  image = "${var.REPOSITORY_URI}:latest"  # Опис реєстру образів
 
-  container {
-    image = "${var.REPOSITORY_URI}:latest" # Використовує останню версію імеджа
-    command = []  # Можна додати команди для запуску контейнера
-    environment = {
-      APP_ENV = "production"
-    }
-    ports = {
-      "80" = "HTTP"
-    }
+  ports {
+    # Consistent with the port exposed by the Dockerfile and app.py
+    8080 = "HTTP"
   }
 
   public_endpoint {
-    container_name = "flask_app"
-    container_port = 80
-
-    health_check {
-      healthy_threshold   = 2
-      unhealthy_threshold = 2
-      timeout_seconds     = 2
-      interval_seconds    = 5
-      path                = "/"
-      success_codes       = "200-499"
-    }
+    container_name = "flask-application"
+    # Consistent with the port exposed by the Dockerfile and app.py
+    container_port = 8080
   }
+
+  health_check {
+    # Визначення точки входу та перевірка працездатності
+    healthy_threshold = 3
+    unhealthy_threshold = 5
+    timeout_seconds = 5
+    interval_seconds = 30
+    path = "/"
+    success_codes = "200-499"
+  }
+}
+
+service_name = aws_lightsail_container_service.flask_application.name
 }
